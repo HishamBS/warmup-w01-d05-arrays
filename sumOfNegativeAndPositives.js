42b9b57e5284195b5f1e55d7a495a9a33081326a
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2019-10-03 09:25:57 +0300

    created the function and solved the empty array bonus

---
+++ sumOfNegativeAndPositives.js
@@ -0,0 +1,30 @@
+function sumOfNandP(arr) 
+{
+    let count = 0 ;
+    let Nsum = 0;
+    let finalArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            count++;
+        }
+        else
+        {
+            Nsum += arr[i];
+        }
+    }
+    if (count && Nsum != null ) 
+    {
+        finalArr = [count , Nsum];
+        console.log(finalArr);
+
+    }
+    else
+    {
+        finalArr = [" " , " "];
+        console.log(finalArr);
+        
+    }
+}
+sumOfNandP([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]);
+sumOfNandP([]);
